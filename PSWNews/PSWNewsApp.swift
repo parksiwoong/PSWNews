91ed8e4f3e0d7c80312fa50352850fc803a21740
parksiwoong <tldnd5656@naver.com>
2022-09-13 22:55:55 +0900

    start

---
+++ PSWNews/PSWNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PSWNewsApp.swift
+//  PSWNews
+//
+//  Created by si woong park on 2022/09/13.
+//
+
+import SwiftUI
+
+@main
+struct PSWNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
